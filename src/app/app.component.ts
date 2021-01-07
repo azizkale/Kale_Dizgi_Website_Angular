002d0e8a9368277b46002e26a70b5b369c90a9ff
azizkale <azizkale@hotmail.com>
2021-01-07 15:55:47 +0100

    getImagesFunctionAdded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+
+const GET_IMAGES = gql`
+  query getImage($path: String) {
+    getImages(path: $path) {
+      id
+      description
+      date
+      index
+    }
+  }
+`;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-apollo-angular';
+  datas = [];
+
+  constructor(public apollo: Apollo) {}
+
+  ngOnInit() {
+    this.apollo
+      .watchQuery<any>({
+        query: GET_IMAGES,
+        variables: {
+          path: 'images/slider',
+        },
+      })
+      .valueChanges.subscribe((data) => {
+        this.datas.push(data.data.getImages);
+      });
+  }
 }
